5be0cee4f42c83dd2e08da8e8ec2a7032bfee5b8
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-05-18 22:06:19 +0500

    feat(css-styling): implement core App component

---
+++ ReactJS CSS Styling of Components/my-app/src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Hello from './Hello.jsx'
+import Bye from './Bye.jsx'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Hello/>
+      <img src={reactLogo} alt="" />
+      <Bye/>
+    </>
+  )
+}
+
+export default App
