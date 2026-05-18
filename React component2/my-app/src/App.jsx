d568c780d5db62fc3e3f3930b826e6a896c47584
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-05-18 22:06:16 +0500

    feat(component-2): implement core App component

---
+++ React component2/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+//import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Hello from './Hello'
+import Bye from './Bye'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Hello/>
+      <img src={reactLogo} width="300px" />
+      <Bye/>
+    </>
+  )
+}
+
+export default App
