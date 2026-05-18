49b2f61026d704c6de48c64a320dcc9480210efe
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-05-18 22:06:16 +0500

    feat(component-2): implement Hello logic

---
+++ React component2/my-app/src/Hello.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function Hello(){
+    const name = "Umar"
+   return(
+        <> 
+            <h3 className='test'>Hello {name}</h3>        
+        </>
+   )
+}
+export default Hello               
