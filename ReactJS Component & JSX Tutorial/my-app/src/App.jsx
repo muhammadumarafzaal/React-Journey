16429fa6cd90d911a1dc7a23e3ad32a6365227e3
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-05-18 22:06:23 +0500

    feat(jsx-tutorial): implement core App component

---
+++ ReactJS Component & JSX Tutorial/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>App component</h1>
+    </>
+  )
+}
+
+export default App
