2d067faa7c70abde9aa65e712cfbe504e1fc6027
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-06-10 18:43:43 +0500

    feat(useeffect): implement App container and main entry point

---
+++ ReactJS useEffect Hook/useeffect/src/App.jsx
@@ -0,0 +1,16 @@
+// import First from "./First"
+// import Timer from "./Timer"
+// import WindowSizeTracker from "./WindowSizeTracker"
+import Users from "./Users"
+function App() {
+  return (
+   <>
+   {/* <First/>
+    <Timer/>
+    <WindowSizeTracker/> */}
+    <Users/>
+   </>
+  )
+}
+
+export default App
