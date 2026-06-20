0ed1da6e34c5b9b30429350ff33fb972bec9dec3
muhammadumarafzaal <umarafzaal182@gmail.com>
2026-06-20 15:01:26 +0500

    feat(form-handling): configure main App entry routing

---
+++ Form Handling/src/App.jsx
@@ -0,0 +1,21 @@
+import SimpleForm from './SimpleForm.jsx';
+import MultiInputForm from './MultiInputForm.jsx';
+import AdvancedForm from './AdvancedForm.jsx';
+import UncontrolledForm from './UncontrolledForm.jsx';
+import BasicValidationForm from './BasicValidationForm.jsx';
+
+function App() {
+  return (
+    <div>
+      <SimpleForm />
+      <MultiInputForm/>
+      <AdvancedForm/>
+      <UncontrolledForm/>
+      <BasicValidationForm/>
+
+    </div>
+  )
+  
+}
+
+export default App
